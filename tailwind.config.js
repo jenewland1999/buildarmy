5d92e1afccddc6e9772970a314852951f34af659
jenewland1999 <github@jenewland.me.uk>
2021-04-03 23:12:12 +0100

    ➕ Add/Enable Tailwind CSS within Next.js
    With aspect-ratio, forms, line-clamp, and typography plugins enabled

---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  purge: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "media", // or 'media' or 'class'
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      primary: {
+        darkest: "#c2c2c2",
+        dark: "#eeeeee",
+        DEFAULT: "#f5f5f5",
+        light: "#fafafa",
+        lightest: "#ffffff",
+      },
+      secondary: {
+        darkest: "#102027",
+        dark: "#263238",
+        DEFAULT: "#37474f",
+        light: "#455a64",
+        lightest: "#62727b",
+      },
+      accent: {
+        darkest: "#c68400",
+        dark: "#ffb300",
+        DEFAULT: "#ffc107",
+        light: "#ffca28",
+        lightest: "#ffe54c",
+      },
+    },
+    fontFamily: {
+      sans: ["Source Sans Pro", ...defaultTheme.fontFamily.sans],
+      display: ["Montserrat", "system-ui", "sans-serif"],
+      body: ["Source Sans Pro", "system-ui", "sans-serif"],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
