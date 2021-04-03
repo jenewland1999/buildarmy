4c6b67a75211dac8c2be1c95ce0c1d2cc5f21bb7
jenewland1999 <github@jenewland.me.uk>
2021-04-03 23:46:41 +0100

    ➕ Add Font Awesome v5 to project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,9 @@
 import type { AppProps } from "next/app";
 
+// Import Font Awesome Icons
+import "../utils/fontawesome";
+
+// Import Tailwind CSS
 import "../styles/globals.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
---
+++ src/utils/fontawesome.ts
@@ -0,0 +1,5 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { faBars, faSearch } from "@fortawesome/free-solid-svg-icons";
+
+library.add(fab, faBars, faSearch);
