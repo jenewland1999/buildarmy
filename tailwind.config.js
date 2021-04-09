488e8d3f8526adc06ea277a923b865fdb8d3dc7e
jenewland1999 <github@jenewland.me.uk>
2021-04-09 13:35:50 +0100

    ⬆️ Upgrade TailwindCSS to v2.1.1
    Enable Tailwind CSS JIT mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,44 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  purge: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-  ],
+  mode: "jit",
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: "media", // or 'media' or 'class'
   theme: {
-    colors: {
-      transparent: "transparent",
-      current: "currentColor",
-      primary: {
-        darkest: "#c2c2c2",
-        dark: "#eeeeee",
-        DEFAULT: "#f5f5f5",
-        light: "#fafafa",
-        lightest: "#ffffff",
+    extend: {
+      backgroundImage: (theme) => ({
+        "homepage-hero": "url('/Hero__img@2x.jpg')",
+      }),
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        primary: {
+          darkest: "#c2c2c2",
+          dark: "#eeeeee",
+          DEFAULT: "#f5f5f5",
+          light: "#fafafa",
+          lightest: "#ffffff",
+        },
+        secondary: {
+          darkest: "#102027",
+          dark: "#263238",
+          DEFAULT: "#37474f",
+          light: "#455a64",
+          lightest: "#62727b",
+        },
+        accent: {
+          darkest: "#c68400",
+          dark: "#ffb300",
+          DEFAULT: "#ffc107",
+          light: "#ffca28",
+          lightest: "#ffe54c",
+        },
       },
-      secondary: {
-        darkest: "#102027",
-        dark: "#263238",
-        DEFAULT: "#37474f",
-        light: "#455a64",
-        lightest: "#62727b",
+      fontFamily: {
+        sans: ["Source Sans Pro", ...defaultTheme.fontFamily.sans],
+        display: ["Montserrat", "system-ui", "sans-serif"],
+        body: ["Source Sans Pro", "system-ui", "sans-serif"],
       },
-      accent: {
-        darkest: "#c68400",
-        dark: "#ffb300",
-        DEFAULT: "#ffc107",
-        light: "#ffca28",
-        lightest: "#ffe54c",
-      },
-    },
-    fontFamily: {
-      sans: ["Source Sans Pro", ...defaultTheme.fontFamily.sans],
-      display: ["Montserrat", "system-ui", "sans-serif"],
-      body: ["Source Sans Pro", "system-ui", "sans-serif"],
     },
   },
   variants: {
