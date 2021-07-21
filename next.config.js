2a3e2d7620b148589e6fbdf57d0cc33f0b2f20c4
jenewland1999 <github@jenewland.me.uk>
2021-07-21 17:52:17 +0100

    :wrench: Configure Next.js rewrites to legacy site

--- next.config.js
+++ next.config.js
@@ -1,3 +1,49 @@
 module.exports = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/shop/products",
+        has: [
+          {
+            type: "query",
+            key: "sale",
+            value: "true",
+          },
+        ],
+        destination: "https://buildarmy.com/sale",
+      },
+      {
+        source: "/shop/:path*",
+        destination: "https://buildarmy.com/shop",
+      },
+      {
+        source: "/contact",
+        destination: "https://buildarmy.com/contact",
+      },
+      {
+        source: "/shipping",
+        destination: "https://buildarmy.com/shipping",
+      },
+      {
+        source: "/about-us/:path*",
+        destination: "https://buildarmy.com/about-us",
+      },
+      {
+        source: "/legal/:path*",
+        destination: "https://buildarmy.com/terms-conditions",
+      },
+      {
+        source: "/returns",
+        destination: "https://buildarmy.com/return-policy",
+      },
+      {
+        source: "/about-us/rewards-system",
+        destination: "https://buildarmy.com/reward-system",
+      },
+      {
+        source: "/account/:path*",
+        destination: "https://buildarmy.com/my-account",
+      },
+    ];
+  },
 };
