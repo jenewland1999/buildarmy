bed50c612ce1e83554f07fe84f4b9f29cb418734
jenewland1999 <github@jenewland.me.uk>
2021-07-25 14:55:03 +0100

    :fire: Remove Apollo temporarily

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,9 @@
-import { ApolloProvider } from "@apollo/client";
-import { useApollo } from "@lib/apollo-client";
 import type { AppProps } from "next/app";
 
 import "../styles/globals.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  const apolloClient = useApollo(pageProps);
-
-  return (
-    <ApolloProvider client={apolloClient}>
-      <Component {...pageProps} />
-    </ApolloProvider>
-  );
+  return <Component {...pageProps} />;
 };
 
 export default MyApp;
