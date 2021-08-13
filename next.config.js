21b87e70617bc265f57126c96d7afb353678788c
jenewland1999 <github@jenewland.me.uk>
2021-08-13 09:22:49 +0100

    :wrench: Re-add Next.js redirects to existing BA website

--- next.config.js
+++ next.config.js
@@ -4,10 +4,57 @@ module.exports = {
     defaultLocale: "en-GB",
   },
   images: {
-    domains: [
-      "buildarmy.local",
-      "buildarmy-6a2222.ingress-earth.easywp.com",
-      "i1.wp.com",
-    ],
+    domains: ["buildarmy-6a2222.ingress-earth.easywp.com"],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/shop/products",
+        has: [
+          {
+            type: "query",
+            key: "sale",
+            value: "true",
+          },
+        ],
+        destination: "https://buildarmy.com/sale",
+      },
+      {
+        source: "/shop/:path*",
+        destination: "https://buildarmy.com/shop",
+      },
+      {
+        source: "/contact",
+        destination: "https://buildarmy.com/contact",
+      },
+      {
+        source: "/shipping",
+        destination: "https://buildarmy.com/shipping",
+      },
+      {
+        source: "/about-us/:path*",
+        destination: "https://buildarmy.com/about-us",
+      },
+      {
+        source: "/legal/:path*",
+        destination: "https://buildarmy.com/terms-conditions",
+      },
+      {
+        source: "/returns",
+        destination: "https://buildarmy.com/return-policy",
+      },
+      {
+        source: "/about-us/rewards-system",
+        destination: "https://buildarmy.com/reward-system",
+      },
+      {
+        source: "/account/:path*",
+        destination: "https://buildarmy.com/my-account",
+      },
+      {
+        source: "/post/6th-buildarmy-image-contest",
+        destination: "https://buildarmy.com/6th-buildarmy-image-contest",
+      },
+    ];
   },
 };
